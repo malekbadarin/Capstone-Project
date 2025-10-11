5aef75045a32650db80e40697345642a8c1a49d6
mabad <malek.badarin@gmail.com>
2025-10-11 10:27:05 +0300

    Fix menu and order menu views (empty lists). Fix nav bar to reflect guest/logged in status. Added main order placement funcionality.

--- capstone/restaurant/urls.py
+++ capstone/restaurant/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('menu/', views.menu, name = 'menu'),
     path('login/', views.Login.as_view(), name = 'login'),
     path('signup/', views.signup, name = 'signup'),
+    path('logout/', views.Logout.as_view(), name = 'logout'),
+    path('order/menu/', views.order_menu, name = "order-menu"),
     path('order/review/<int:order_id>', views.orderreview, name = 'order-review'), #order review, edit, and placement view
-    path('order/confirmation/<int:order_id>', views.OrderConfirmation.as_view(), name = 'order-confirmation'),
+    path('order/confirmation/<int:order_id>', views.order_confirmation, name = 'order-confirmation'),
 ]
--- capstone/restaurant/views.py
+++ capstone/restaurant/views.py
@@ -1,13 +1,11 @@
-import django.views.generic as gen_views
-from urllib import request
 from django.shortcuts import render, redirect
 from .models import Menu, Order
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-paths_list = [
+guest_paths_list = [
     'home',
     'about',
     'menu',
@@ -15,21 +13,27 @@ paths_list = [
     'signup',
 ]
 
+auth_paths_list = [
+    'home',
+    'about',
+    'order-menu',
+]
+
 def home(request):
-    return render(request, 'index.html', {'paths': paths_list})
+    return render(request, 'index.html', {'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list})
 
 def about(request):
-    return render(request, 'about.html', {'paths': paths_list})
+    return render(request, 'about.html', {'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list})
 
 def menu(request):
-    return render(request, 'restaurant/menu.html', {'menu': Menu.objects.all(), 'paths': paths_list})
+    return render(request, 'restaurant/menu.html', {'menu': Menu.objects.all(), 'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list})
 
 class Login(LoginView):
     template_name = 'registration/login.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['paths'] = paths_list
+        context['paths'] = guest_paths_list
         return context
     
 def signup(request):
@@ -44,12 +48,27 @@ def signup(request):
         else:
             error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
-    context = {'form': form, 'paths': paths_list, 'error_message': error_message}
+    context = {'form': form, 'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+class Logout(LogoutView):
+    next_page = '/'
+
+def order_menu(request):
+    try: #checks if there is an Open order for the user. If not, creates one
+        order = Order.objects.filter(user = request.user, status = 'O')[0]
+    except:
+        order = Order(user = request.user)
+        order.save()
+    return render(request, 'restaurant/order_menu.html', {'menu': Menu.objects.all(), 'order': order, 'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list})
 
 def orderreview(request, order_id):
     order = Order.objects.filter(id = order_id)[0] #Select the first -and only- item in the query set
-    return render(request, 'restaurant/order_review.html', {'paths': paths_list, 'order': order})
+    return render(request, 'restaurant/order_review.html', {'order': order, 'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list, 'order': order})
 
-class OrderConfirmation(gen_views.DetailView):
-    pass
+def order_confirmation(request, order_id):
+    order = Order.objects.filter(id = order_id)[0] #Select the first -and only- item in the query set
+    if order.status == 'O': #Changes the status of the order to Placed instead of Open
+        order.status = 'P'
+        order.save()
+    return render(request, 'restaurant/order_confirmation.html', {'guest_paths': guest_paths_list, 'auth_paths': auth_paths_list, 'order': order})
